89a0234b5128976593e0859d868c5d66c48f7e3b
chelseakno <iamcmknox@gmail.com>
2021-06-23 14:35:57 -0500

    Adding comment about api key.

--- static/js/config.js
+++ static/js/config.js
@@ -1,2 +1,2 @@
-// Public api key provided by website for general public use
+// Public api key provided by mapbox website for general public use
 const api_key = 'pk.eyJ1IjoibWFwYm94IiwiYSI6ImNpejY4NXVycTA2emYycXBndHRqcmZ3N3gifQ.rJcFIG214AriISLbB6B5aw'
